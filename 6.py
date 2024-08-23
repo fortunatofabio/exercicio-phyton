86e0b667a7574da3c296803995a76394df3f0066
fortunatofabio <fabiofortunato958@gmail.com>
2024-08-23 20:24:38 -0300

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Digite a medida da base "))
+numero2 = int(input("Digite a medida da altura "))
+Resultado=numero1*numero2
+print("a area do seu retangulo é" , numero1*numero2 )
