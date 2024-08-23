a5a819c40f6a6308953d308e7698746e83fbf393
fortunatofabio <fabiofortunato958@gmail.com>
2024-08-23 20:25:09 -0300

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,5 @@
+ano = int(input("Digite em que ano você nasceu:  "))
+mes = int(input("Digite o Mês do seu nascimento:  "))
+dia = int(input("Digite o dia do seu nascimento: "))
+anomesdia = (ano*365) + (mes*30) + dia
+print("Sua idade em dias é:", anomesdia)
